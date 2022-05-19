8472f645900a4ca20a384b942990f0b97acbcff9
sandrohrj <sandrohrj@gmail.com>
2022-05-18 21:18:12 -0300

    Bytebank

---
+++ Bytebank/index.js
@@ -0,0 +1,41 @@
+class Cliente{
+    nome;
+    cpf;
+}
+
+class ContaCorrente{
+    agencia;
+    saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
+}
+
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233344;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 22222233344;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(80);
+console.log(contaCorrenteRicardo.saldo);
+
+/*const contaCorrenteAlice = new ContaCorrente();
+contaCorrenteAlice.saldo = 0;
+contaCorrenteAlice.agencia = 1001;*/
+
+console.log(cliente1);
+console.log(cliente2);
+
