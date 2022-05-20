edb4908b3ef31c07417fc645548d3510e62d5107
sandrohrj <sandrohrj@gmail.com>
2022-05-20 06:34:49 -0300

    Get, Set e Constructor

--- Bytebank/ContaCorrente.js
+++ Bytebank/ContaCorrente.js
@@ -2,6 +2,7 @@ import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente {
     agencia;
+<<<<<<< HEAD
     _cliente;
 
     set cliente(novoValor) {
@@ -13,10 +14,14 @@ export class ContaCorrente {
     get cliente() {
         return this._cliente;
     }
+=======
+    cliente;
+>>>>>>> b3effd1eb5c7377d4681de6bb3cb048bb88f2e57
 
     // _saldo = 0 https://github.com/tc39/proposal-class-fields#private-fields
     _saldo = 0;
 
+<<<<<<< HEAD
     get saldo(){
         return this._saldo;
     }
@@ -42,6 +47,24 @@ export class ContaCorrente {
     }
 
     transferir(valor, conta) {
+=======
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+        
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+            return;
+        }
+        this._saldo += valor;
+    }
+
+    transferir(valor, conta){
+>>>>>>> b3effd1eb5c7377d4681de6bb3cb048bb88f2e57
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
     }
--- Bytebank/index.js
+++ Bytebank/index.js
@@ -1,9 +1,12 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
+<<<<<<< HEAD
 
 const cliente1 = new Cliente("Ricardo", 11122233344);
 
 const cliente2 = new Cliente("Alice", 22222233344);
+=======
+>>>>>>> b3effd1eb5c7377d4681de6bb3cb048bb88f2e57
 
 const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 //contaCorrenteRicardo.agencia = 1001;
@@ -14,13 +17,34 @@ const contaCorrenteAlice = new ContaCorrente(cliente2, 1001);
 //contaCorrenteAlice.agencia = 1001;
 //contaCorrenteAlice.cliente = cliente2;
 
+<<<<<<< HEAD
 console.log("\r\n*********************************************\r\n");
 console.log(contaCorrenteAlice);
 console.log(contaCorrenteRicardo);
 
+=======
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
+
+const contaCorrenteAlice = new ContaCorrente();
+contaCorrenteAlice.agencia = 1001;
+contaCorrenteAlice.cliente = cliente2;
+
+console.log("\r\n*********************************************\r\n");
+console.log(contaCorrenteAlice);
+console.log(contaCorrenteRicardo);
+
+>>>>>>> b3effd1eb5c7377d4681de6bb3cb048bb88f2e57
 let valor = 200;
 contaCorrenteRicardo.transferir(valor, contaCorrenteAlice);
 
 console.log("\r\n*********************************************\r\n");
 console.log(contaCorrenteAlice);
+<<<<<<< HEAD
 console.log(contaCorrenteRicardo);
+=======
+console.log(contaCorrenteRicardo);
+console.log("valor: ", valor);
+>>>>>>> b3effd1eb5c7377d4681de6bb3cb048bb88f2e57
